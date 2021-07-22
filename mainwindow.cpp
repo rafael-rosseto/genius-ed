ef82a8e13516e8e5bb0161134aed3a1a613a3ef0
Rafael Rosseto <rafaelbassirosseto@gmail.com>
2021-07-22 17:18:35 -0300

    add efeitos de luz

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QTime>
+/*
+setStyleSheet("QPushButton" "{" "background-color : lightblue;" "}"
+              "QPushButton::pressed" "{" "background-color : red;" "}"
+             );*/
 
 int level = 3;
 
@@ -19,10 +23,18 @@ MainWindow::MainWindow(QWidget *parent)
 
     ui->play->clearFocus();
 
-    ui->amarelo->setStyleSheet("background: yellow");
-    ui->verde->setStyleSheet("background: green");
-    ui->vermelho->setStyleSheet("background: red");
-    ui->azul->setStyleSheet("background: blue");
+    ui->amarelo->setStyleSheet("QPushButton" "{" "background-color : rgb(240,240,0);" "}"
+                               "QPushButton::pressed" "{" "background-color : rgb(255,255,150);" "}"
+                              );
+    ui->verde->setStyleSheet("QPushButton" "{" "background-color : green;" "}"
+                             "QPushButton::pressed" "{" "background-color : rgb(150,255,150);" "}"
+                            );
+    ui->vermelho->setStyleSheet("QPushButton" "{" "background: red;" "}"
+                                "QPushButton::pressed" "{" "background: rgb(255,150,150);" "}"
+                               );
+    ui->azul->setStyleSheet("QPushButton" "{" "background-color : blue;" "}"
+                            "QPushButton::pressed" "{" "background-color : rgb(150,150,255);" "}"
+                           );
 }
 
 MainWindow::~MainWindow()
@@ -34,45 +46,45 @@ void MainWindow::on_play_clicked()
 {
     ui->play->clearFocus();
 
-    ui->amarelo->setStyleSheet("background: palette(window)");
-    ui->verde->setStyleSheet("background: palette(window)");
-    ui->vermelho->setStyleSheet("background: palette(window)");
-    ui->azul->setStyleSheet("background: palette(window)");
-
     delay(800);
 
     for(int i = 1; i <= level; i++)
     {
-        //for(int j = 1; j <= i; j++)
-        //{
-            srand(time(0));
-            int aux = rand() % 4;
-            if(aux == 0)
-            {
-                ui->amarelo->setStyleSheet("background: yellow");
-                delay(600);
-                ui->amarelo->setStyleSheet("background: palette(window)");
-            }
-            else if(aux == 1)
-            {
-                ui->verde->setStyleSheet("background: green");
-                delay(600);
-                ui->verde->setStyleSheet("background: palette(window)");
-            }
-            else if(aux == 2)
-            {
-                ui->vermelho->setStyleSheet("background: red");
-                delay(600);
-                ui->vermelho->setStyleSheet("background: palette(window)");
-            }
-            else
-            {
-                ui->azul->setStyleSheet("background: blue");
-                delay(600);
-                ui->azul->setStyleSheet("background: palette(window)");
-            }
+        srand(time(0));
+        int aux = rand() % 4;
+        if(aux == 0)
+        {
+            ui->amarelo->setStyleSheet("background: rgb(255,255,150)");
             delay(600);
-        //}
+            ui->amarelo->setStyleSheet("QPushButton" "{" "background-color : rgb(240,240,0);" "}"
+                                       "QPushButton::pressed" "{" "background-color : rgb(255,255,150);" "}"
+                                      );
+        }
+        else if(aux == 1)
+        {
+            ui->verde->setStyleSheet("background: rgb(150,255,150)");
+            delay(600);
+            ui->verde->setStyleSheet("QPushButton" "{" "background-color : green;" "}"
+                                     "QPushButton::pressed" "{" "background-color : rgb(150,255,150);" "}"
+                                    );
+        }
+        else if(aux == 2)
+        {
+            ui->vermelho->setStyleSheet("background: rgb(255,150,150)");
+            delay(600);
+            ui->vermelho->setStyleSheet("QPushButton" "{" "background: red;" "}"
+                                        "QPushButton::pressed" "{" "background: rgb(255,100,100);" "}"
+                                       );
+        }
+        else
+        {
+            ui->azul->setStyleSheet("background: rgb(150,150,255)");
+            delay(600);
+            ui->azul->setStyleSheet("QPushButton" "{" "background-color : blue;" "}"
+                                    "QPushButton::pressed" "{" "background-color : rgb(150,150,255);" "}"
+                                   );
+        }
+        delay(600);
     }
 }
 
@@ -80,8 +92,4 @@ void MainWindow::on_play_clicked()
 void MainWindow::on_reset_clicked()
 {
     ui->reset->clearFocus();
-    ui->amarelo->setStyleSheet("background: yellow");
-    ui->verde->setStyleSheet("background: green");
-    ui->vermelho->setStyleSheet("background: red");
-    ui->azul->setStyleSheet("background: blue");
 }
